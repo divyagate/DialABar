3acbf0543635e25ea074d82b08500fc9f915c5d2
divya <gatedivya123@gmail.com>
2020-10-26 13:46:39 -0400

    cardinfo page and OrderPlaced

---
+++ app/src/main/java/com/example/dialabar/CardInformation.java
@@ -0,0 +1,14 @@
+package com.example.dialabar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CardInformation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_card_information);
+    }
+}
